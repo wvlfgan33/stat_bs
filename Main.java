04835b1d0ee37f94d7d33fff0a8005835cdb4e75
Remus <remusyap.yap90@gmail.com>
2024-05-17 10:35:52 +0800

    TODO: Please start using the BigDecimal class...

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        CSVFile csvfile = new CSVFile(new File("HHData.csv"));
 
-        ArrayList<String> X = csvfile.getDataColumn("famsize");
-        ArrayList<String> Y = csvfile.getDataColumn("ressize");
+        System.out.println(SpecialFunctions.erf(1));
+        
+        //CSVFile csvfile = new CSVFile(new File("HHData.csv"));
 
-        System.out.println(Operations.covariance(X, Y));
-        System.out.println(Operations.median(X));
+        //ArrayList<String> X = csvfile.getDataColumn("famsize");
+        //ArrayList<String> Y = csvfile.getDataColumn("ressize");
+
+        //System.out.println(Operations.covariance(X, Y));
+        //System.out.println(Operations.median(X));
     }
 
 }
